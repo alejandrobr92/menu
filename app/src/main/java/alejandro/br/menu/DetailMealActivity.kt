902b498ba9030d75eef30b56dc30b87abc1f7e8e
alejandro <alejandro.reyes@alumno.uttehuacan.edu.mx>
2020-06-19 02:54:51 -0500

    Splash screen

---
+++ app/src/main/java/alejandro/br/menu/DetailMealActivity.kt
@@ -0,0 +1,12 @@
+package alejandro.br.menu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailMealActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_meal)
+    }
+}
---
+++ app/src/main/java/alejandro/br/menu/Splash.kt
@@ -0,0 +1,12 @@
+package alejandro.br.menu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Splash : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
