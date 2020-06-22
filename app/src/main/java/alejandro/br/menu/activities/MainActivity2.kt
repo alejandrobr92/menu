08da8fe5352cc609add111876e93568c28bd0e6f
alejandro <alejandro.reyes@alumno.uttehuacan.edu.mx>
2020-06-22 16:47:19 -0500

    nav_host

--- app/src/main/java/alejandro/br/menu/activities/MainActivity2.kt
+++ app/src/main/java/alejandro/br/menu/activities/MainActivity2.kt
@@ -3,10 +3,32 @@ package alejandro.br.menu.activities
 import alejandro.br.menu.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var navController : NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        findViewById<BottomNavigationView>(R.id.bottom_navigation)
+            .setupWithNavController(navController)
     }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
+        //findNavController(R.id.nav_host_fragment).navigateUp()
+        // return super.onSupportNavigateUp()
+    }
+
 }
