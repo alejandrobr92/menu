89b49433c6f82e15e0000a1c18b00f6cf3e52dd8
alejandro <alejandro.reyes@alumno.uttehuacan.edu.mx>
2020-06-23 14:14:14 -0500

    Some cleaning and files deleting, the fisrt stable point

--- app/src/main/java/alejandro/br/menu/activities/MainActivity2.kt
+++ app/src/main/java/alejandro/br/menu/activities/MainActivity2.kt
@@ -3,6 +3,9 @@ package alejandro.br.menu.activities
 import alejandro.br.menu.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -10,6 +13,7 @@ import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main2.*
 
 class MainActivity2 : AppCompatActivity() {
 
@@ -18,6 +22,8 @@ class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        setSupportActionBar(toolbar)
 
         val navController = findNavController(R.id.nav_host_fragment)
         findViewById<BottomNavigationView>(R.id.bottom_navigation)
@@ -30,5 +36,26 @@ class MainActivity2 : AppCompatActivity() {
         //findNavController(R.id.nav_host_fragment).navigateUp()
         // return super.onSupportNavigateUp()
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId){
+            R.id.settings ->{
+                Toast.makeText(this, "Ir a configuración", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.my_history ->{
+                Toast.makeText(this,"Ir a mi historial", Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
 
 }
